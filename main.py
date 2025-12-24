ad79fa10bf6dc7ae93c2d23306ce697342beb884
jeremy liu <jeremyliu621@gmail.com>
2025-12-24 11:32:21 -0500

    reference page and ufc_events.csv from it created

--- main.py
+++ main.py
@@ -4,6 +4,32 @@ import pandas as pd
 import time
 
 UFCStats_BaseURL = "http://ufcstats.com/statistics/events/completed?page="
-page = requests.get(UFCStats_BaseURL)
 
-soup = BeautifulSoup(page.content, "html.parser")
+def main():
+    events_list = []
+    current_page_number = 1
+    has_more_pages = True
+    while has_more_pages:
+        soup = get_page_html(current_page_number)
+        print(page(soup))
+        has_more_pages = False
+
+def page(soup):
+    # finds all events on a page
+    page_events = soup.find_all("a", class_="b-link b-link_style_black")
+    # exits when all events have been exhausted
+    if not page_events:
+        return False
+    else:
+        for event in page_events:
+            print(event)
+
+
+# since the page has multiple pages, needs a function that can parse repeatedly, according to page #
+def get_page_html(page_number):
+    page = requests.get(UFCStats_BaseURL + str(page_number))
+    
+    return BeautifulSoup(page.content, "html.parser")
+
+main()
+
