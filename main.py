81e8aefe3d9db0f8d82cbd9af7eac2aacbd769b9
jeremy liu <jeremyliu621@gmail.com>
2025-12-24 11:49:25 -0500

    understood more

--- main.py
+++ main.py
@@ -6,30 +6,37 @@ import time
 UFCStats_BaseURL = "http://ufcstats.com/statistics/events/completed?page="
 
 def main():
+
+    # 1) scrapes all events
     events_list = []
     current_page_number = 1
-    has_more_pages = True
-    while has_more_pages:
-        soup = get_page_html(current_page_number)
-        print(page(soup))
-        has_more_pages = False
 
-def page(soup):
-    # finds all events on a page
-    page_events = soup.find_all("a", class_="b-link b-link_style_black")
-    # exits when all events have been exhausted
-    if not page_events:
-        return False
-    else:
-        for event in page_events:
-            print(event)
-
+    # while True:
+    soup = get_page_html(current_page_number)
+    page_events = parse_page(soup)
+    
+    print(page_events)
 
 # since the page has multiple pages, needs a function that can parse repeatedly, according to page #
 def get_page_html(page_number):
     page = requests.get(UFCStats_BaseURL + str(page_number))
-    
     return BeautifulSoup(page.content, "html.parser")
 
-main()
+def parse_page(soup):
+    # finds all events on a page
+    page_events = soup.find_all("a", class_="b-link b-link_style_black")
+    # exits when all events have been exhausted
+    events = []
+    for event in page_events:
+        name = event.get_text(strip=True)
+        link = event.get("href")
+        events.append({
+            "event_name": name,
+            "event_url": link
+        })
+
+    return events
+
+if __name__ == "__main__":
+    main()
 
