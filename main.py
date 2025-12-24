4d53732f34f2aa06f888c48f930d1f9dfb7a5a6e
jeremy liu <jeremyliu621@gmail.com>
2025-12-24 13:50:50 -0500

    added the dict for all wanted info

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ UFCStats_BaseURL = "http://ufcstats.com/statistics/events/completed?page="
 def main():
 
     # 1) scrapes all events
-    events_list = []
+    all_events = []
     current_page_number = 1
 
     while True:
@@ -21,25 +21,41 @@ def main():
             break
         
         # adds all the page from "current_page_number" to all page_events
-        events_list.extend(page_events)
+        all_events.extend(page_events)
         print(
             f"Page {current_page_number}: "
             f"{len(page_events)} events "
-            f"(total: {len(events_list)})"
+            f"(total: {len(all_events)})"
         )
 
         # continues to next iteration
-        current_page_number += 1
+        #current_page_number += 1
         time.sleep(0.1)
+        break # remove later
 
     print(
         "Finished scraping all events on the main page."
-        "Created 'event_name' and 'event_url' in events_list"
+        "Created 'event_name' and 'event_url' in all_events"
         )
+    # convert all_events into a pandas dataframe
+    events_df = pd.DataFrame(all_events)
+    print("convert all_events into a pandas dataframe")
 
-    
+    # 2) scrape ALL fights from ALL events within each event's fighter
 
-    # 2) 
+    all_fights = []
+
+    event_count = len(all_events)
+    current_event = 1
+
+    for event in all_events:
+        # get 'event_name' and 'event
+        event_name = event["event_name"]
+        event_url = event["event_url"]
+
+        event_soup = get_event_soup(event_url)
+        fights = parse_fights_from_event(event_name, event_url, event_soup)
+
 
 
 
@@ -64,6 +80,100 @@ def parse_page(soup):
         })
 
     return events
+
+def get_event_soup(event_url):
+    specific_event_soup = requests.get(event_url)
+    return BeautifulSoup(specific_event_soup.content, "html.parser")
+
+def parse_fights_from_event(event_name, event_url, event_soup):
+    specific_event_fights = []
+
+    # find the table that contains all the fights
+    table_body = event_soup.find("tbody")
+    if table_body is None:
+        print("Can not find fights within a certain event.")
+        return specific_event_fights
+    
+    # each table row is one fight
+    fight_rows = table_body.find_all("tr")
+
+    for fight_row in fight_rows:
+        row_columns = fight_row.find_all("td")
+
+        # checks for redundant row_columns
+        if len(row_columns) < 10:
+            print("redundant column in row_column check found.")
+            continue
+
+        fighter_1_result = row_columns[0].get_text(strip=True)
+
+        '''
+        find_all("p") due to HTML tree:
+        <tr>
+            <td>W</td>
+            <td>
+                <p>Jon Jones</p>
+                <p>Daniel Cormier</p>
+            </td>
+        </tr>
+        '''
+
+        # fighter names
+        fighter_1_name, fighter_2_name = sort_two_paragraphs(row_columns[1])
+
+        # fighter KD/STR/TD/SUB
+        fighter_1_knockdowns_number, fighter_2_knockdowns_number = sort_two_paragraphs(row_columns[2])
+        fighter_1_number_strikes, fighter_2_number_strikes = sort_two_paragraphs(row_columns[3])
+        fighter_1_number_takedowns, fighter_2_number_takedowns = sort_two_paragraphs(row_columns[4])
+        fighter_1_number_submissions, fighter_2_number_submissions = sort_two_paragraphs(row_columns[5])
+
+        # weightclass
+        weightclass = row_columns[6].find_all("p")
+
+        # method + move
+        method, move = sort_two_paragraphs(row_columns[7])
+
+        # round + time
+        round_that_won = row_columns[8].get_text(strip=True)
+        time_elapsed_in_winround = row_columns[9].get_text(strip=True)
+
+        specific_event_fights.append({
+            "event": event_name,
+            "event_url": event_url,
+
+            "fighter_1_result": fighter_1_result,
+
+            "fighter_1_name": fighter_1_name,
+            "fighter_2_name": fighter_2_name,
+
+            "fighter_1_knockdowns_number": fighter_1_knockdowns_number,
+            "fighter_2_knockdowns_number": fighter_2_knockdowns_number,
+
+            "fighter_1_number_strikes": fighter_1_number_strikes,
+            "fighter_2_number_strikes": fighter_2_number_strikes,
+
+            "fighter_1_number_takedowns": fighter_1_number_takedowns,
+            "fighter_2_number_takedowns": fighter_2_number_takedowns,
+
+            "fighter_1_number_submissions": fighter_1_number_submissions,
+            "fighter_2_number_submissions": fighter_2_number_submissions,
+
+            "weightclass": weightclass,
+
+            "method": method,
+            "move": move,
+
+            "round_that_won": round_that_won,
+            "time_elapsed_in_winround": time_elapsed_in_winround
+        })
+    
+
+def sort_two_paragraphs(row_column):
+
+    row_column.find_all("p")
+    fighter_1_stat = row_column[0].get_text(strip=True)
+    fighter_2_stat = row_column[1].get_text(strip=True)
+    return fighter_1_stat, fighter_2_stat
 
 if __name__ == "__main__":
     main()
